538feb32b585b9c5793b94d78d8f78a36e2855ec
anmolshl <anmolshl2@gmail.com>
2017-02-02 19:18:43 -0800

    Moving camera with movement bug

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+    public GameObject player;
+	// Use this for initialization
+	void Start () {
+        this.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10);
+    }
+}
