020f7b276999c50ba906cecaed83e664b614b5f1
Raphael Chang Menoni <raphaelchangmenoni@dhcp-128-189-206-190.ubcsecure.wireless.ubc.ca>
2017-02-02 18:47:38 -0800

    Now rotates and changes mvmnt direction

--- Assets/Scripts/PlayerMover.cs
+++ Assets/Scripts/PlayerMover.cs
@@ -11,12 +11,16 @@ public class PlayerMover : MonoBehaviour {
     public float speed;
     public float turnSpeed;
     // Use this for initialization
+	private const float DEG_TO_RAD = Mathf.PI / 180.0f;
     private Vector3 forwardVec = new Vector3(1, 0, 0);
+	private float angle = 0.0f;
+
     void Start () {
 		
 	}
     void FixedUpdate()
     {
+		//this.transform.rotation.eulerAngles
         if (Input.GetKey(up))
         {
             Vector3 forward = speed * forwardVec;
@@ -30,11 +34,18 @@ public class PlayerMover : MonoBehaviour {
 
         if (Input.GetKey(right))
         {
-            transform.Rotate(Vector3.left * turnSpeed);
+            //transform.Rotate(Vector3.left * turnSpeed);
+			transform.Rotate(0.0f, 0.0f, -turnSpeed);
+			angle = (angle - turnSpeed) % 360.0f;
+			forwardVec.Set (Mathf.Cos (angle * DEG_TO_RAD), Mathf.Sin (angle * DEG_TO_RAD), 0);
+
         }
         else if (Input.GetKey(left))
         {
-            transform.Rotate(Vector3.right * turnSpeed);
+			transform.Rotate(0.0f, 0.0f, turnSpeed);
+			angle = (angle + turnSpeed) % 360.0f;
+			forwardVec.Set (Mathf.Cos (angle * DEG_TO_RAD), Mathf.Sin (angle * DEG_TO_RAD), 0);
+
         }
     }
     // Update is called once per frame
