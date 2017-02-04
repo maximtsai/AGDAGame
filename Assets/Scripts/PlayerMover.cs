bffece53233c5e2cd271ecfa12bc596e78d2018f
Maxim Tsai <Maxim Tsai>
2017-02-04 14:13:20 -0800

    added gitignore file to ignore Library/ files, also replaced physics with 2D components and fixed rotation bug which wass simply caused by really low rotational drag

--- Assets/Scripts/PlayerMover.cs
+++ Assets/Scripts/PlayerMover.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMover : MonoBehaviour {
-    public Rigidbody playerBody;
+    Rigidbody2D playerBody;
     public KeyCode up;
     public KeyCode down;
     public KeyCode left;
@@ -12,40 +12,35 @@ public class PlayerMover : MonoBehaviour {
     public float turnSpeed;
     // Use this for initialization
 	private const float DEG_TO_RAD = Mathf.PI / 180.0f;
-    private Vector3 forwardVec = new Vector3(1, 0, 0);
-	private float angle = 0.0f;
+    private Vector2 forwardVec = new Vector3(1, 0);
 
     void Start () {
-		
-	}
+        playerBody = GetComponent<Rigidbody2D>();
+    }
     void FixedUpdate()
     {
 		//this.transform.rotation.eulerAngles
         if (Input.GetKey(up))
         {
-            Vector3 forward = speed * forwardVec;
+            Vector2 forward = speed * forwardVec;
             playerBody.AddForce(forward);
         }
         else if (Input.GetKey(down))
         {
-            Vector3 backward = -1 * speed * forwardVec;
+            Vector2 backward = -1 * speed * forwardVec;
             playerBody.AddForce(backward);
         }
 
         if (Input.GetKey(right))
         {
-            //transform.Rotate(Vector3.left * turnSpeed);
-			transform.Rotate(0.0f, 0.0f, -turnSpeed);
-			angle = (angle - turnSpeed) % 360.0f;
-			forwardVec.Set (Mathf.Cos (angle * DEG_TO_RAD), Mathf.Sin (angle * DEG_TO_RAD), 0);
-
+            playerBody.AddTorque(-turnSpeed, ForceMode2D.Force);
+            //playerBody.Rotate(0.0f, 0.0f, -turnSpeed);
+			forwardVec.Set(Mathf.Cos (playerBody.rotation * DEG_TO_RAD), Mathf.Sin (playerBody.rotation * DEG_TO_RAD));
         }
         else if (Input.GetKey(left))
         {
-			transform.Rotate(0.0f, 0.0f, turnSpeed);
-			angle = (angle + turnSpeed) % 360.0f;
-			forwardVec.Set (Mathf.Cos (angle * DEG_TO_RAD), Mathf.Sin (angle * DEG_TO_RAD), 0);
-
+            playerBody.AddTorque(turnSpeed, ForceMode2D.Force);// (0.0f, 0.0f, turnSpeed);
+			forwardVec.Set(Mathf.Cos (playerBody.rotation * DEG_TO_RAD), Mathf.Sin (playerBody.rotation * DEG_TO_RAD));
         }
     }
     // Update is called once per frame
