403310706ace8502277b9e1228091e98ec143650
Maxim Tsai <Maxim Tsai>
2017-02-04 18:48:27 -0800

    moved FollowPlayer script and made player movement more 'epic'

--- Assets/Scripts/PlayerMover.cs
+++ Assets/Scripts/PlayerMover.cs
@@ -14,7 +14,7 @@ public class PlayerMover : MonoBehaviour {
     // Use this for initialization
 	private const float DEG_TO_RAD = Mathf.PI / 180.0f;
     private Vector2 forwardVec = new Vector3(1, 0);
-
+    private bool forwardPressed = false;
     void Start () {
         playerBody = GetComponent<Rigidbody2D>();
     }
@@ -22,33 +22,58 @@ public class PlayerMover : MonoBehaviour {
     {
         if (Input.GetKey(up))
         {
+            forwardPressed = true;
             Vector2 forward = acceleration * forwardVec;
             if (playerBody.velocity.sqrMagnitude > maxSpeed)
             {
+                // soft cap for max speed
                 forward = forward * (maxSpeed+15 - playerBody.velocity.sqrMagnitude)/ 15;
             }
-            Debug.Log(playerBody.velocity.sqrMagnitude);
             playerBody.AddForce(forward);
         }
+        else
+        {
+            forwardPressed = false;
+        }
+        /*
         else if (Input.GetKey(down))
         {
+            forwardPressed = true;
             Vector2 backward = -1 * acceleration * forwardVec;
             if (playerBody.velocity.sqrMagnitude > maxSpeed)
             {
+                // soft cap for max speed
                 backward = backward * (maxSpeed+10 - playerBody.velocity.sqrMagnitude) / 10;
             }
             playerBody.AddForce(backward);
         }
+        */
+
 
         if (Input.GetKey(right))
         {
-            playerBody.AddTorque(-turnSpeed, ForceMode2D.Force);
+            if (forwardPressed)
+            {
+                // turn slower while moving
+                playerBody.AddTorque(-turnSpeed * 0.5f, ForceMode2D.Force);
+            } else
+            {
+                playerBody.AddTorque(-turnSpeed, ForceMode2D.Force);
+            }
             //playerBody.Rotate(0.0f, 0.0f, -turnSpeed);
-			forwardVec.Set(Mathf.Cos (playerBody.rotation * DEG_TO_RAD), Mathf.Sin (playerBody.rotation * DEG_TO_RAD));
+            forwardVec.Set(Mathf.Cos (playerBody.rotation * DEG_TO_RAD), Mathf.Sin (playerBody.rotation * DEG_TO_RAD));
         }
         else if (Input.GetKey(left))
         {
-            playerBody.AddTorque(turnSpeed, ForceMode2D.Force);// (0.0f, 0.0f, turnSpeed);
+            if (forwardPressed)
+            {
+                // turn slower while moving
+                playerBody.AddTorque(turnSpeed * 0.5f, ForceMode2D.Force);
+            }
+            else
+            {
+                playerBody.AddTorque(turnSpeed, ForceMode2D.Force);
+            }
 			forwardVec.Set(Mathf.Cos (playerBody.rotation * DEG_TO_RAD), Mathf.Sin (playerBody.rotation * DEG_TO_RAD));
         }
     }
