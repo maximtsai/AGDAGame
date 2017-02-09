99b98f9e87d40e37633e72b54bbdc1e4f1973609
Maxim Tsai <Maxim Tsai>
2017-02-08 20:12:29 -0800

    added weapon

---
+++ Assets/WeaponBehavior.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponBehavior : MonoBehaviour {
+    private GameObject playerObject;
+    private float distToPlayer;
+    // Use this for initialization
+    void Start () {
+        playerObject = GameObject.FindWithTag("Player");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        // Debug.Log(Vector2.Distance(playerObject.transform.position, this.transform.position ));
+	}
+}
