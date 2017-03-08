009ab27d805a58d55ad36febcd20e1c7aa09ba49
anmolshl <anmolshl2@gmail.com>
2017-03-07 19:07:51 -0800

    Flare in progress...

---
+++ Assets/SparkSystem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SparkSystem : MonoBehaviour {
+    public GameObject flares;
+
+    void Start()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            Instantiate(flares, contact.otherCollider.transform, true);
+        }
+        //if (collision.relativeVelocity.magnitude > 2)
+        
+
+    }
+}
