e8d05a90c6581725b111fa4b39fc3a7b798e82eb
Maxim Tsai <Maxim Tsai>
2017-03-27 12:31:00 -0700

    blood in collision, made arena, tweaks to player health

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -7,32 +7,67 @@ public class PlayerHealth : MonoBehaviour {
 	public int dmgVelocity;
 	public int playerHealth;
     float initialDeltaTime = 0;
+    ParticleSystem blood;
 
-
-	// Use this for initialization
-	void Start () {
-		Debug.Log ("Initial health: " + playerHealth);
+    // Use this for initialization
+    void Start () {
         initialDeltaTime = Time.fixedDeltaTime;
-
-    }
-	
-	void OnCollisionEnter2D(Collision2D collision) {
-		//Debug.Log();
-		Debug.Log(this.gameObject.tag);
-		Debug.Log(collision.gameObject.tag);
-        if (collision.gameObject.CompareTag("Player"))
+        foreach (Transform childObj in this.transform)
         {
-            if (collision.relativeVelocity.magnitude >= dmgVelocity)
+            if (childObj.CompareTag("BloodSystem"))
             {
-                Debug.Log("Remaining health: " + playerHealth);
-                playerHealth -= (int)Mathf.Abs(Time.fixedDeltaTime / initialDeltaTime*Vector2.Dot(collision.contacts[0].normal, collision.relativeVelocity));
-                Time.timeScale = 0.03f;
+                blood = childObj.GetComponent<ParticleSystem>();
+                break;
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        //Debug.Log();
+        // what u collided with Debug.Log(collision.collider.gameObject.name);
+        // playear Debug.Log(this.gameObject.tag);
+        if (!collision.contacts[0].otherCollider.gameObject.CompareTag("Weapon"))
+        {
+            float dotImpact = Vector2.Dot(collision.contacts[0].normal, collision.relativeVelocity);
+            if (collision.collider.gameObject.CompareTag("Weapon"))
+            {
+                dotImpact = dotImpact * 2;
+                Debug.Log("new impact: " + dotImpact);
+            }
+            float playerMass = this.GetComponent<Rigidbody2D>().mass;
+            Rigidbody2D colliderRB = collision.contacts[0].collider.GetComponent<Rigidbody2D>();
+            float colliderMass = 9999;
+            if (colliderRB)
+            {
+                colliderMass = colliderRB.mass;
+            }
+            // more massive objects you hit hurt you more. More massive players receive slightly less damage, slightly.
+            float massRatio = colliderMass / (colliderMass + Mathf.Max(1, playerMass * 0.1f));
+            dotImpact *= massRatio;
+            if (Mathf.Abs(dotImpact) >= dmgVelocity)
+            {
+
+                int damageDealt = (int)Mathf.Max(1, (Time.fixedDeltaTime / initialDeltaTime * (Mathf.Abs(dotImpact) - dmgVelocity)));
+                Debug.Log("damageDealt: "+ damageDealt);
+                playerHealth -= damageDealt;
+                Debug.Log("playerHealth: " + playerHealth);
+                Time.timeScale = Mathf.Max((12 - (float)damageDealt) / 15, 0.03f);
                 Time.fixedDeltaTime = initialDeltaTime * Time.timeScale;
+
+                int bloodEmitted = (int)Mathf.Max(1, damageDealt * 0.25f);
+                blood.Emit(bloodEmitted);
+                blood.transform.position = collision.contacts[0].point;
             }
             if (playerHealth <= 0)
             {
                 gameObject.SetActive(false);
             }
+        }
+        
+		// what you collided with Debug.Log(collision.gameObject.name);
+        if (collision.gameObject.CompareTag("Player"))
+        {
+
         }
 	}
 }
