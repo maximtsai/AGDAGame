189e5b72df9e871b3adbad2af7070091cd343aa3
Raphael Chang Menoni <raphael.menoni@gmail.com>
2017-03-30 18:19:03 -0700

    rocket stuff

---
+++ Assets/Scripts/FlareOnCollision.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlareOnCollision : MonoBehaviour {
+
+    public GameObject flares;
+    public GameObject player;
+    public GameObject player1;
+
+    void Start()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision.contacts);
+        foreach (ContactPoint2D contact in collision.contacts)
+        {
+            Debug.Log(this.gameObject.name);
+            Instantiate(flares, this.gameObject.transform, true);
+            Vector3 flarePos = new Vector3(contact.point.x, contact.point.y);
+            flares.transform.position = flarePos;
+        }
+        //if (collision.relativeVelocity.magnitude > 2)
+    }
+}
---
+++ Assets/Scripts/RocketController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RocketController : MonoBehaviour {
+
+	public float rocketDuration = 5f;
+	public GameObject explosion;
+
+	private Vector3 position;
+
+	// Initialize the private fields
+	void Awake()
+	{
+		position = transform.position;
+	}
+
+	// Set the timer for the explosion
+	void Start () {
+		Invoke("Explode", rocketDuration);
+	}
+
+	// Explode as soon as missile hits some collider box
+	void OnCollisionEnter(Collision collision) {
+		Explode();
+	}
+
+	// Destroy object after limit time or after collision
+	void Explode() {
+		CancelInvoke("Explode");
+		Instantiate(explosion, position, Quaternion.identity);
+		Destroy(gameObject);
+	}
+}
