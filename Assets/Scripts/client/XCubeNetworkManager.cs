971430240bd55ef59b78980af7684fd1578187a6
leesunny <dev@leesunny.com>
2017-05-12 14:12:20 -0700

    Created server scene and added copies of server/client scripts

---
+++ Assets/Scripts/client/XCubeNetworkManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//First get access to the DarkRift namespace
+using DarkRift;
+
+public class XCubeNetworkManager : MonoBehaviour
+{
+	public string IP = "127.0.0.1";
+
+	void Start()
+	{
+		DarkRiftAPI.Connect(IP);
+	}
+
+	void OnApplicationQuit()
+	{
+		DarkRiftAPI.Disconnect();
+	}
+}
